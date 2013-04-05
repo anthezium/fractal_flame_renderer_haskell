23d4532fb2cf5212fa99a33b6d1bee37f1371e1c
Ted Cooper <anthezium@gmail.com>
2013-04-05 00:52:30 -0400

    Completed variations listed on wiki, refactored args to getVps

--- FractalFlame/Variation.hs
+++ FractalFlame/Variation.hs
@@ -113,7 +113,8 @@ gaussianRandom psis =
 -- look at flam3-render to see if this should be truncate
 (%) a b = fromIntegral $ (round a) `mod` (round b)
 
-getVps vparams names = map (vparams !) names
+getVps :: VParams -> String -> [String] -> [Coord]
+getVps vparams prefix = map ((vparams !) . ((prefix ++ "_") ++))
 
 -- Variations
 linear (VarP {point}) = point
@@ -235,14 +236,14 @@ blur (VarP {psi1, psi2}) =
     fmap (* psi1) $ Point (cos z) (sin z)
 
 rings2 (VarP {vparams, r, theta}) =
-  let [v] = getVps vparams ["rings2_val"]
+  let [v] = getVps vparams "rings2" ["val"]
       p = v^2
       t = r - 2 * p * trunc ((r + p) / (2 * p)) + r * (1 - p)
   in
     fmap (* t) $ Point (sin theta) (cos theta)
 
 fan2 (VarP {vparams, r, theta}) =
-  let [vx, vy] = getVps vparams ["fan2_x", "fan2_y"]
+  let [vx, vy] = getVps vparams "fan2" ["x", "y"]
       p1 = pi * vx^2
       p2 = vy
       t = theta + p2 - p1 * trunc (2 * theta * p2 / p1)
@@ -254,24 +255,24 @@ fan2 (VarP {vparams, r, theta}) =
       g $ theta + p1 / 2
 
 blob (VarP {vparams, r, theta}) =
-  let [high, low, waves] = getVps vparams ["blob_high", "blob_low", "blob_waves"]
+  let [high, low, waves] = getVps vparams "blob" ["high", "low", "waves"]
       m = r * (low + ((high - low) / 2) * (sin (waves * theta) + 1))
   in
     fmap (* m) $ Point (cos theta) (sin theta)
 
 pdj (VarP {vparams, x, y}) =
-  let [a,b,c,d] = getVps vparams ["pdj_a", "pdj_b", "pdj_c", "pdj_d"]
+  let [a,b,c,d] = getVps vparams "pdj" ["a", "b", "c", "d"]
   in
     Point ((sin $ a * y) - (cos $ b * x)) ((sin $ c * x) - (cos $ d * y)) 
 
 perspective (VarP {vparams, x, y}) =
-  let [angle, dist] = getVps vparams ["perspective_angle", "perspective_dist"]
+  let [angle, dist] = getVps vparams "perspective" ["angle", "dist"]
       m = dist / (dist - y * sin angle)
   in
     fmap (* m) $ Point x (y * cos angle)
 
 julian (VarP {psi1, vparams, phi, r}) =
-  let [power, dist] = getVps vparams ["julian_power", "julian_dist"]
+  let [power, dist] = getVps vparams "julian" ["power", "dist"]
       p = trunc $ (abs power) * psi1
       t = (phi + 2 * pi * p) / power
       m = r**(dist / power)
@@ -279,7 +280,7 @@ julian (VarP {psi1, vparams, phi, r}) =
     fmap (* m) $ Point (cos t) (sin t)
 
 juliascope (VarP {psi1, lambda1, vparams, phi, r}) =
-  let [power, dist] = getVps vparams ["juliascope_power", "juliascope_dist"]
+  let [power, dist] = getVps vparams "juliascope" ["power", "dist"]
       p = trunc $ (abs power) * psi1
       t = (lambda1 * phi + 2 * pi * p) / power
       m = r**(dist / power)
@@ -294,7 +295,7 @@ gaussian (VarP {psi1, gaussianR}) =
 -- ask spot about 1) is ra the same as rA on the wiki? 2) why doesn't the code on the wiki divide the final X and Y by W?
 -- following spec in the paper for now
 radialblur (VarP {gaussianR, vparams, weight, x, y, r, phi}) =
-  let [angle] = getVps vparams ["radialblur_angle"]
+  let [angle] = getVps vparams "radialblur" ["angle"]
       p = angle * pi / 2
       t1 = weight * gaussianR
       t2 = phi + t1 * sin p
@@ -303,14 +304,14 @@ radialblur (VarP {gaussianR, vparams, weight, x, y, r, phi}) =
     fmap (/ weight) $ Point (r * cos t2 + t3 * x) (r * sin t2 + t3 * y)
 
 pie (VarP {psi1, psi2, psi3, vparams}) =
-  let [slices, rotation, thickness] = getVps vparams ["pie_slices", "pie_rotation", "pie_thickness"]
+  let [slices, rotation, thickness] = getVps vparams "pie" ["slices", "rotation", "thickness"]
       t1 = trunc $ psi1 * slices + 0.5
       t2 = rotation + 2 * pi / slices * (t1 + psi2 * thickness)
   in
     fmap (* psi3) $ Point (cos t2) (sin t2)
 
 ngon (VarP {vparams, phi, r, point}) =
-  let [power, sides, corners, circle] = getVps vparams ["ngon_power", "ngon_sides", "ngon_corners", "ngon_circle"]
+  let [power, sides, corners, circle] = getVps vparams "ngon" ["power", "sides", "corners", "circle"]
       p = 2 * pi / sides
       t3 = phi - p * flr (phi / p)
       t4 = if t3 > p / 2 then t3 else t3 - p
@@ -319,7 +320,7 @@ ngon (VarP {vparams, phi, r, point}) =
     fmap (* k) $ point  
 
 curl (VarP {vparams, x, y}) =
-  let [c1, c2] = getVps vparams ["curl_c1", "curl_c2"]
+  let [c1, c2] = getVps vparams "curl" ["c1", "c2"]
       t1 = 1 + c1 * x + c2 * (x^2 - y^2)
       t2 = c1 * y + 2 * c2 * x * y
       m = 1 / (t1^2 + t2^2)
@@ -327,7 +328,7 @@ curl (VarP {vparams, x, y}) =
     fmap (* m) $ Point (x * t1 + y * t2) (y * t1 - x * t2)
 
 rectangles (VarP {vparams, x, y}) =
-  let [rx, ry] = getVps vparams ["rectangles_x", "rectangles_y"]
+  let [rx, ry] = getVps vparams "rectangles" ["x", "y"]
   in
     Point ((2 * flr (x / rx) + 1) * rx - x) ((2 * flr (y / ry) + 1) * ry - y)
 
@@ -369,13 +370,61 @@ twintrain (VarP {psi1, weight, r, x}) =
   in
     fmap (* x) $ Point t (t - pi * sin z)
 
--- did following without wiki, FIX ORDER
 cross (VarP {x, y, point}) =
   let m = sqrt $ 1 / (x^2 - y^2)^2
   in
     fmap (* m) point
 
--- are there additional variations on the wiki but not in the paper?
+disc2 (VarP {vparams, x, y, phi}) =
+  let [rot, twist] = getVps vparams "disc2" ["rot", "twist"]
+      rtp = rot * pi
+      k = case twist of 
+            twist | twist > 2 * pi     -> 1 + twist - 2 * pi
+                  | twist < (- 2) * pi -> 1 + twist + 2 * pi
+                  | otherwise          -> 1
+      st = k * (sin twist)
+      ct = k * (cos twist)
+      t = rtp * (x + y)
+      srt = sin t
+      crt = cos t
+      m = phi / pi
+  in    
+    fmap (* m) $ Point (srt + ct) (crt + st)
+
+supershape (VarP {vparams, r, theta, psi1, point}) =
+  let [m, n1, n2, n3, holes, rnd] = getVps vparams "supershape" ["m", "n1", "n2", "n3", "holes", "md"]
+      pm_4 = m / 4
+      pneg1_n1 = (-1.0) / n1
+      th = pm_4 * theta + pi / 4
+      t1 = (**n2) . abs . sin $ th
+      t2 = (**n3) . abs . cos $ th 
+      mx = (rnd * psi1 + (1 - rnd) * r - holes) * (t1 + t2)**pneg1_n1 / r
+  in
+    fmap (* mx) point
+
+flower (VarP {vparams, theta, psi1}) =
+  let [holes, petals] = getVps vparams "flower" ["holes", "petals"]
+      m = (psi1 - holes) * (cos $ petals * theta)
+  in
+    fmap (* m) $ Point (cos theta) (sin theta)
+
+conic (VarP {vparams, theta, psi1}) =
+  let [holes, eccen] = getVps vparams "conic" ["holes", "petals"]
+      m = (psi1 - holes) * eccen / (1 + eccen * (cos theta))
+  in
+    fmap (* m) $ Point (cos theta) (sin theta)
+
+parabola (VarP {vparams, r, psi1, psi2}) =
+  let [height, width] = getVps vparams "parabola" ["height", "width"]
+  in
+    Point (height * (sin r)^2 * psi1) (width * (cos r) * psi2)
+
+bent2 (VarP {vparams, x, y}) =
+  let [vx, vy] = getVps vparams "bent2" ["x", "y"]
+      x' = if x < 0 then x * vx else x
+      y' = if y < 0 then y * vy else y
+  in
+    Point x' y'
 
 -- gotta be a better way to do this
 vars = [
@@ -427,5 +476,10 @@ vars = [
   , ("secant2", secant2)
   , ("twintrain", twintrain)
   , ("cross", cross)
+  , ("disc2", disc2)
+  , ("supershape", supershape)
+  , ("conic", conic)
+  , ("parabola", parabola)
+  , ("bent2", bent2)
   ]
   
